62a0afa9b6f6d9615a3ad7d140f6207213500eef
prashant <csprshantphuse@gmail.com>
2021-09-23 12:55:14 +0530

    Practice

--- SpringSecurityUsingJWT/src/main/java/com/nt/filter/JwtRequestFilter.java
+++ SpringSecurityUsingJWT/src/main/java/com/nt/filter/JwtRequestFilter.java
@@ -33,7 +33,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		logger.trace("JwtRequestFilter.doFilterInternal()====");
+		logger.trace("JwtRequestFilter.doFilterInternal()=888");
 		
 		final String authorizationHeader=request.getHeader("Authorization");
 		System.out.println(authorizationHeader);
